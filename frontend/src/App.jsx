ed27a38a01e702b6651e1c36c5f876083237da07
Arul0107 <149875230+Arul0107@users.noreply.github.com>
2025-03-08 19:21:50 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AppLayout from "./components/sidebar/AppLayout";
 import Dashboard from "./components/dashboard/Dashboard";
-import User from "./components/userprev/user";
+import User from "./components/userprev/User";
 
 const App = () => {
   return (
